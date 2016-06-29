d25217cc0290da0d8b8255d7ca182002b5ce7efb
raphael meyer <raphael.akira.meyer@gmail.com>
2016-06-29 22:38:31 +0200

    Spike to post

--- redraspi.py
+++ redraspi.py
@@ -1,7 +1,14 @@
-import config
-import twitter
+#!/bin/python
+
+def take_picture():
+    import subprocess
+
+    subprocess.run(['raspistill', '-o', '/tmp/picture.jpg', '-hf', '-vf', '-w', '256', '-h', '256', '-t', '1000'])
 
 def main():
+    import config
+    import twitter
+
     api = twitter.Api(
             consumer_key = config.twitter.consumer_key,
             consumer_secret = config.twitter.consumer_secret,
@@ -9,7 +16,11 @@ def main():
             access_token_secret = config.twitter.access_secret,
             input_encoding = 'utf-8')
 
-    print(api.GetUserTimeline(screen_name='pimoroni'))
+    take_picture()
+
+    status = api.PostMedia(status = 'My first post. Weeha!', media='/tmp/picture.jpg')
+
+    print(status)
 
 if __name__ == "__main__":
     main()
