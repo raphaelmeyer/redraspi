d13c06f9e1c00ec555aaaef3a748f7b1fb093576
raphael meyer <raphael.akira.meyer@gmail.com>
2016-06-29 21:53:30 +0200

    test app

---
+++ redraspi.py
@@ -0,0 +1,18 @@
+import config
+import twitter
+
+def main():
+    api = twitter.Api(
+            consumer_key = config.twitter.consumer_key,
+            consumer_secret = config.twitter.consumer_secret,
+            access_token_key = config.twitter.access_token,
+            access_token_secret = config.twitter.access_secret,
+            input_encoding = 'utf-8')
+
+    print(api.GetUserTimeline(screen_name='pimoroni'))
+
+
+if __name__ == "__main__":
+    main()
+
+
