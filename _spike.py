8eb53fff282aa2bbc06e61959c6ac52078b9098e
Raphael Meyer <raphael.meyer@bbv.ch>
2016-08-04 08:23:37 +0200

    Move example code from spike into its own file

---
+++ _spike.py
@@ -0,0 +1,17 @@
+#
+# results, examples, knowledge etc from spikes
+#
+
+#def post_tweet():
+#    from settings import settings
+#    import twitter
+#
+#    api = twitter.Api(
+#            consumer_key = settings.consumer_key,
+#            consumer_secret = settings.consumer_secret,
+#            access_token_key = settings.access_token,
+#            access_token_secret = settings.access_secret,
+#            input_encoding = 'utf-8')
+#
+#    api.PostUpdate()
+
--- redraspi/redraspi.py
+++ redraspi/redraspi.py
@@ -15,16 +15,3 @@ def take_picture():
     cam = camera.Camera()
     cam.take_picture()
 
-#def post_tweet():
-#    from settings import settings
-#    import twitter
-#
-#    api = twitter.Api(
-#            consumer_key = settings.consumer_key,
-#            consumer_secret = settings.consumer_secret,
-#            access_token_key = settings.access_token,
-#            access_token_secret = settings.access_secret,
-#            input_encoding = 'utf-8')
-#
-#    api.PostUpdate()
-
