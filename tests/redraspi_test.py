9ed75e57f50ac9b48b376219dfa015f2ff956901
Raphael Meyer <raphael.meyer@bbv.ch>
2016-08-04 08:20:32 +0200

    Clean up test

--- tests/redraspi_test.py
+++ tests/redraspi_test.py
@@ -1,27 +1,28 @@
 from redraspi import redraspi
 from unittest import mock
 
+@mock.patch('redraspi.sensors.Sensors')
 @mock.patch('redraspi.camera.Camera')
 class TestRedRasPi:
-    def test_does_not_take_a_picture_when_too_dark(self, camera_mock):
-        with mock.patch('redraspi.sensors.Sensors') as Sensors:
-            sensors = Sensors.return_value
-            sensors.brightness.return_value = 0.69
 
-            testee = redraspi.RedRasPi()
-            testee.start()
+    def setup(self):
+        self.testee = redraspi.RedRasPi()
 
-            cam = camera_mock.return_value
-            cam.take_picture.assert_not_called()
+    def test_does_not_take_a_picture_when_too_dark(self, Camera, Sensors):
+        sensors = Sensors.return_value
+        sensors.brightness.return_value = 0.69
 
-    def test_takes_a_picture_when_it_is_bright_enough(self, camera_mock):
-        with mock.patch('redraspi.sensors.Sensors') as Sensors:
-            sensors = Sensors.return_value
-            sensors.brightness.return_value = 0.71
+        self.testee.start()
 
-            testee = redraspi.RedRasPi()
-            testee.start()
+        camera = Camera.return_value
+        camera.take_picture.assert_not_called()
 
-            cam = camera_mock.return_value
-            cam.take_picture.assert_called_once_with()
+    def test_takes_a_picture_when_it_is_bright_enough(self, Camera, Sensors):
+        sensors = Sensors.return_value
+        sensors.brightness.return_value = 0.71
+
+        self.testee.start()
+
+        camera = Camera.return_value
+        camera.take_picture.assert_called_once_with()
 
