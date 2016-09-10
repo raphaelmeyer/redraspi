d27c181b24eae2e86717d26b6f7ad016a7412c0a
Raphael Meyer <raphael.meyer@bbv.ch>
2016-09-10 08:26:12 +0200

    Extract main loop logic into its own class

---
+++ TODO.md
@@ -0,0 +1,16 @@
+# TODO
+
+## Next steps
+
+- start/stop functions
+- start shall call a 'loop' function every few (m)s
+
+
+
+
+
+
+
+
+
+
--- redraspi/redraspi.py
+++ redraspi/redraspi.py
@@ -1,17 +1,21 @@
-#!/bin/python
-
 from redraspi import sensors, camera
+
+class Main:
+    def __init__(self):
+        self.sensors = sensors.Sensors()
+        self.camera = camera.Camera()
+    def loop(self):
+        if self.sensors.brightness() > 0.7:
+            self.take_picture()
+
+    def take_picture(self):
+        self.camera.take_picture()
 
 class RedRasPi:
     def start(self):
-        sens = sensors.Sensors()
-        if sens.brightness() > 0.7:
-            take_picture()
+        main = Main()
+        main.loop()
 
     def stop(self):
         pass
-
-def take_picture():
-    cam = camera.Camera()
-    cam.take_picture()
 
