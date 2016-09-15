1600c0a01ceac73f3b5f2327df621ae443b1911e
Raphael Meyer <raphael.meyer@bbv.ch>
2016-09-15 21:04:44 +0200

    Update todo list

--- tests/redraspi_test.py
+++ tests/redraspi_test.py
@@ -1,11 +1,20 @@
 from redraspi import redraspi
 from unittest import mock
+import unittest
 
 @mock.patch('redraspi.redraspi.Main')
 class TestRedRasPi:
 
     def setup(self):
         self.testee = redraspi.RedRasPi()
+
+    @unittest.skip("pending")
+    def test_calls_main_loop_every_100ms(self, Main):
+        self.fail()
+
+    @unittest.skip("pending")
+    def test_stops_main_loop_execution(self, Main):
+        self.fail()
 
     def test_calls_main_loop_when_started(self, Main):
         self.testee.start()
